af18666bb54232c73258b78c8485b628dd09e104
wael-boudissaa <jw_boudissa@esi.dz>
2024-07-23 14:00:48 +0100

    tt
    ttt

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import TestGit from "@/components/home/TestGit";
 import Welcome from "@/components/home/Welcome";
 import NavBar from "@/components/NavBar";
 import { useState } from "react";
@@ -7,8 +8,9 @@ export default function Home() {
   const [thiss, setThis] = useState<number>(3);
   return (
     <section className="flex flex-col ">
-      <NavBar />
-      <Welcome />
+      {/* <NavBar /> */}
+      {/* <Welcome /> */}
+      <TestGit />
       <div></div>
     </section>
   );
